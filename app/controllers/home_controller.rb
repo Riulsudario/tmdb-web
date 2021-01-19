9fb120ecd62187f6f3267fed319ecd898df29305
riulsudario <riulsudario@jera.com.br>
2021-01-18 21:06:26 -0400

    [NEW] adding routes and views to controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    redirect_to dashboard_path if user_signed_in?
+
   end
 
   def dashboard
