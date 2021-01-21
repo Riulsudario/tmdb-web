d61d8d0ffc34f2a14683a0f3f2ffe5e24c8d4046
riulsudario <riulsudario@jera.com.br>
2021-01-21 13:45:42 -0400

    [FIX] removing byebug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   require 'themoviedb'
 
   before_action :set_config
-  byebug
   Tmdb::Api.key(Rails.application.credentials.dig(Rails.env.to_sym, :tmdb_api_key))
 
   def set_config
