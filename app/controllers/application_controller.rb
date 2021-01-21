a553ec3a6c9640a315cf53fb13c315b640903640
riulsudario <riulsudario@jera.com.br>
2021-01-21 11:52:39 -0400

    [NEW] tmdb api and gem setup and integration and dashboard view created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  require 'themoviedb'
+
+  before_action :set_config
+  Tmdb::Api.key(Rails.application.credentials.dig(Rails.env.to_sym, :tmdb_api_key))
+
+  def set_config
+  	@configuration = Tmdb::Configuration.new
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
 
   def index
     redirect_to dashboard_path if user_signed_in?
-
   end
 
   def dashboard
+    @movies = Tmdb::Movie.now_playing
   end
 end
