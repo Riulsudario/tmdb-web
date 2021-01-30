42636274e7021583c85e2a54a9254f0ac3b0a9c5
riulsudario <riulsudario@jera.com.br>
2021-01-30 17:23:25 -0400

    [NEW] similar movies container and component created

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,44 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user!
+	before_action :authenticate_user!
+	before_action :movie_params
+	before_action :movie
+	before_action :images
+	before_action :cast
+	before_action :trailers
+	before_action :similar_movies
 
-  def show
-  	@movie = Tmdb::Movie.detail(params[:id])
-  	@images = Tmdb::Movie.images(params[:id])
-  	@cast = Tmdb::Movie.casts(params[:id])
-  	@trailers = Tmdb::Movie.trailers(params[:id])
-		@similar_movies = Tmdb::Movie.similar_movies(params[:id])
-  end
+	def show
+	end
+
+	private
+
+	def movie_params
+		params.permit(:id)
+	end
+
+	def movie
+		@movie = Tmdb::Movie.detail(movie_params[:id])
+		@movie if @movie.present?
+	end
+
+	def images
+		@images = Tmdb::Movie.images(movie_params[:id])
+		@images if @images.present?
+	end
+
+	def cast
+		@cast = Tmdb::Movie.casts(movie_params[:id])
+		@cast if @cast.present?
+	end
+
+	def trailers
+		@trailers = Tmdb::Movie.trailers(movie_params[:id])
+		@trailers if @trailers.present?
+	end
+
+	def similar_movies
+		@similar_movies = Tmdb::Movie.similar_movies(movie_params[:id])
+		@similar_movies if @similar_movies.present?
+	end
+
 end
